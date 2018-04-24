44b9e94b046969e255c2d59008bae68987eaad53
Luke Nzang'u <saintlukke@gmail.com>
2018-04-24 23:27:41 +0300

    rename bamApi to api.py

--- test/test_bam_api.py
+++ test/test_bam_api.py
@@ -3,19 +3,18 @@ import sys
 #when using python to run tests this is needed
 sys.path.insert(0, '/home/ludaone/andela/book-a-meal')
 
-from app.bamApi import app
+from app.api import app
 import json
 
 class BamApiTestCase(unittest.TestCase):
 
     def setup(self):
         app.testing = True
-        self.app = app.test_client()
-
+        self.apiApp = app.test_client()
 
     # Basic test for a status 200 on index route
     def test_index(self):
-        response = self.app.get('/')
+        response = self.apiApp.get('/')
         result = json.loads(response.data)
 
         self.assertEqual(response.status_code, 200)
